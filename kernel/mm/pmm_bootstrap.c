870b76ea7e24d4005129ee96246b61b08e1a6577
godrenkon <renkomkun@gmail.com>
2026-08-28 07:51:09 +0900

    mm: move bootstrap PMM pool into reserved high RAM window

--- kernel/mm/pmm_bootstrap.c
+++ kernel/mm/pmm_bootstrap.c
@@ -1,8 +1,8 @@
 #include "pmm.h"
 
-#define PMM_MAX_PAGES (64u * 1024u * 1024u / SB_PAGE_SIZE)
-#define PMM_FIRST_PAGE (96u * 1024u * 1024u / SB_PAGE_SIZE)
-#define PMM_LAST_PAGE  (120u * 1024u * 1024u / SB_PAGE_SIZE)
+#define PMM_MAX_PAGES (256u * 1024u * 1024u / SB_PAGE_SIZE)
+#define PMM_FIRST_PAGE (192u * 1024u * 1024u / SB_PAGE_SIZE)
+#define PMM_LAST_PAGE  (224u * 1024u * 1024u / SB_PAGE_SIZE)
 
 static uint64_t page_count;
 static uint64_t free_count;
