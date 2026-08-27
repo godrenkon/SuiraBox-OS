f4cc861c60a8d10eff59ac3dbabb2e9cdbaf0aa4
godrenkon <renkomkun@gmail.com>
2026-08-27 18:04:02 +0900

    mm: add Multiboot2 memory-map definitions

---
+++ kernel/mm/multiboot_memory.h
@@ -0,0 +1,29 @@
+#ifndef SB_MULTIBOOT_MEMORY_H
+#define SB_MULTIBOOT_MEMORY_H
+
+#include <stdint.h>
+
+#define MULTIBOOT2_TAG_TYPE_END        0u
+#define MULTIBOOT2_TAG_TYPE_MMAP       6u
+#define MULTIBOOT2_MMAP_TYPE_AVAILABLE 1u
+
+struct multiboot2_tag {
+    uint32_t type;
+    uint32_t size;
+};
+
+struct multiboot2_mmap_tag {
+    uint32_t type;
+    uint32_t size;
+    uint32_t entry_size;
+    uint32_t entry_version;
+};
+
+struct multiboot2_mmap_entry {
+    uint64_t addr;
+    uint64_t len;
+    uint32_t type;
+    uint32_t reserved;
+};
+
+#endif
