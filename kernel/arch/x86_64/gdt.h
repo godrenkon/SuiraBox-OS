bf10d1d2b1475da690b3a80b6375c296173da1a3
godrenkon <renkomkun@gmail.com>
2026-08-27 18:27:04 +0900

    arch: add GDT and TSS interface

---
+++ kernel/arch/x86_64/gdt.h
@@ -0,0 +1,15 @@
+#ifndef SB_ARCH_X86_64_GDT_H
+#define SB_ARCH_X86_64_GDT_H
+
+#include <stdint.h>
+
+#define SB_KERNEL_DATA_SELECTOR 0x10u
+#define SB_KERNEL_CODE_SELECTOR 0x18u
+#define SB_USER_CODE_SELECTOR   0x23u
+#define SB_USER_DATA_SELECTOR   0x2Bu
+#define SB_TSS_SELECTOR         0x30u
+
+void gdt_init(void);
+void gdt_set_kernel_stack(uint64_t stack_pointer);
+
+#endif
