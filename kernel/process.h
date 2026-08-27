b4b1b4e6e53142294d6c0d3c57366bf4ba86dad6
godrenkon <renkomkun@gmail.com>
2026-08-27 18:22:08 +0900

    process: attach isolated address space to process

--- kernel/process.h
+++ kernel/process.h
@@ -2,6 +2,7 @@
 #define SB_KERNEL_PROCESS_H
 
 #include <stdint.h>
+#include "mm/address_space.h"
 
 #define SB_MAX_PROCESSES 32u
 #define SB_MAX_THREADS_PER_PROCESS 16u
@@ -26,6 +27,7 @@ typedef struct {
     sb_process_state_t state;
     uint32_t thread_count;
     sb_thread_t threads[SB_MAX_THREADS_PER_PROCESS];
+    sb_address_space_t address_space;
 } sb_process_t;
 
 void process_init(void);
@@ -33,5 +35,7 @@ sb_process_t *process_create(uint64_t pid);
 sb_thread_t *process_create_thread(sb_process_t *process, uint64_t tid, uint32_t priority);
 sb_process_t *process_get(uint64_t pid);
 uint32_t process_count(void);
+int process_activate(sb_process_t *process);
+void process_destroy(sb_process_t *process);
 
 #endif
