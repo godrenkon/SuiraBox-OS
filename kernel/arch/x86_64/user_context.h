5715630302f43b8d8f0070e5eefd7d1cf2548a92
godrenkon <renkomkun@gmail.com>
2026-08-27 18:38:12 +0900

    Add userspace CPU context model

---
+++ kernel/arch/x86_64/user_context.h
@@ -0,0 +1,30 @@
+#ifndef SB_ARCH_X86_64_USER_CONTEXT_H
+#define SB_ARCH_X86_64_USER_CONTEXT_H
+
+#include <stdint.h>
+
+/* Register state required when a userspace thread is resumed. */
+typedef struct {
+    uint64_t r15;
+    uint64_t r14;
+    uint64_t r13;
+    uint64_t r12;
+    uint64_t r11;
+    uint64_t r10;
+    uint64_t r9;
+    uint64_t r8;
+    uint64_t rsi;
+    uint64_t rdi;
+    uint64_t rbp;
+    uint64_t rdx;
+    uint64_t rcx;
+    uint64_t rbx;
+    uint64_t rax;
+    uint64_t rip;
+    uint64_t cs;
+    uint64_t rflags;
+    uint64_t rsp;
+    uint64_t ss;
+} sb_user_context_t;
+
+#endif
