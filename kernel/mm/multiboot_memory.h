8f34b162f5b2d67ba006e77bb5f56adf534c0eea
godrenkon <renkomkun@gmail.com>
2026-08-27 18:38:49 +0900

    Define Multiboot2 module tag

--- kernel/mm/multiboot_memory.h
+++ kernel/mm/multiboot_memory.h
@@ -4,12 +4,21 @@
 #include <stdint.h>
 
 #define MULTIBOOT2_TAG_TYPE_END        0u
+#define MULTIBOOT2_TAG_TYPE_MODULE     3u
 #define MULTIBOOT2_TAG_TYPE_MMAP       6u
 #define MULTIBOOT2_MMAP_TYPE_AVAILABLE 1u
 
 struct multiboot2_tag {
     uint32_t type;
     uint32_t size;
+};
+
+struct multiboot2_module_tag {
+    uint32_t type;
+    uint32_t size;
+    uint32_t mod_start;
+    uint32_t mod_end;
+    char string[0];
 };
 
 struct multiboot2_mmap_tag {
