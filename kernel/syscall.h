0b80253dffe596d1adde072e54f3a65cef363d7d
godrenkon <renkomkun@gmail.com>
2026-08-27 18:16:27 +0900

    syscall: define initial system call interface

---
+++ kernel/syscall.h
@@ -0,0 +1,14 @@
+#ifndef SB_KERNEL_SYSCALL_H
+#define SB_KERNEL_SYSCALL_H
+
+#include <stdint.h>
+
+#define SB_SYS_GET_TICKS  0u
+#define SB_SYS_PROCESS_ID 1u
+#define SB_SYS_EXIT       2u
+
+void syscall_init(void);
+uint64_t syscall_dispatch(uint64_t number, uint64_t arg0, uint64_t arg1,
+                          uint64_t arg2, uint64_t arg3, uint64_t arg4);
+
+#endif
