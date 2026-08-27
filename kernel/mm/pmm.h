8b7ee0cc59ddb1ee279f4e5a130cf8caf059a90c
godrenkon <renkomkun@gmail.com>
2026-08-27 18:04:20 +0900

    mm: expose Multiboot-aware PMM initialization

--- kernel/mm/pmm.h
+++ kernel/mm/pmm.h
@@ -6,6 +6,10 @@
 #define SB_PAGE_SIZE 4096u
 
 void pmm_init(uint64_t usable_start, uint64_t usable_end);
+void pmm_init_from_multiboot(uint64_t multiboot_info_address);
+void pmm_reset(void);
+void pmm_add_usable_range(uint64_t usable_start, uint64_t usable_end);
+void pmm_reserve_range(uint64_t start, uint64_t end);
 void *pmm_alloc_page(void);
 void pmm_free_page(void *page);
 uint64_t pmm_total_pages(void);
