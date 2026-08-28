359f6f4563ff37172398508261557568534aad39
godrenkon <renkomkun@gmail.com>
2026-08-28 22:44:48 +0900

    feat(gui): define input and glyph syscalls

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -9,6 +9,8 @@
 #define SB_SYS_DISPLAY_INFO    3u
 #define SB_SYS_DISPLAY_CLEAR   4u
 #define SB_SYS_DISPLAY_RECT    5u
+#define SB_SYS_INPUT_KEY       6u
+#define SB_SYS_DISPLAY_GLYPH   7u
 
 void syscall_init(void);
 uint64_t syscall_dispatch(uint64_t number, uint64_t arg0, uint64_t arg1,
