26eb13969acc8105f810db4abc08095551cc6779
godrenkon <renkomkun@gmail.com>
2026-08-29 06:06:41 +0900

    feat(shell): expose command names through stable ABI

--- userspace/shell.h
+++ userspace/shell.h
@@ -5,6 +5,7 @@
 
 /* Stable userspace shell command ABI. */
 uint64_t sb_shell_command_count(void);
+const char *sb_shell_command_name(uint64_t index);
 uint64_t sb_shell_run_command(uint64_t index);
 
 #endif
