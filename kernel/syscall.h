f2477a0039a4eba81f985cf24e739f1c28f78aeb
godrenkon <renkomkun@gmail.com>
2026-08-28 22:59:45 +0900

    feat(input): add mouse packet syscall ABI

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -11,6 +11,7 @@
 #define SB_SYS_DISPLAY_RECT    5u
 #define SB_SYS_INPUT_KEY       6u
 #define SB_SYS_DISPLAY_GLYPH   7u
+#define SB_SYS_INPUT_MOUSE     8u
 
 void syscall_init(void);
 uint64_t syscall_dispatch(uint64_t number, uint64_t arg0, uint64_t arg1,
