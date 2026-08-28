5e292460d03d69a7ec96a96d836ef31b965b3b71
godrenkon <renkomkun@gmail.com>
2026-08-28 21:07:42 +0900

    feat: add virtual framebuffer mapping state

--- kernel/framebuffer.h
+++ kernel/framebuffer.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+#define SB_FRAMEBUFFER_VIRTUAL_BASE 0x0000005000000000ull
+
 typedef struct {
     uint64_t address;
     uint32_t pitch;
@@ -16,11 +18,14 @@ typedef struct {
     uint8_t green_mask_size;
     uint8_t blue_position;
     uint8_t blue_mask_size;
+    uint64_t mapped_address;
+    uint64_t mapped_size;
 } sb_framebuffer_info_t;
 
 int sb_framebuffer_init(uint64_t multiboot_info_address);
 int sb_framebuffer_available(void);
 const sb_framebuffer_info_t *sb_framebuffer_info(void);
+int sb_framebuffer_map(void);
 int sb_framebuffer_clear(uint8_t red, uint8_t green, uint8_t blue);
 
 #endif
