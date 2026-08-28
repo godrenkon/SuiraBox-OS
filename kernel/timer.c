c8af1b759399001d35a0c143a4e53d07e2e457b8
godrenkon <renkomkun@gmail.com>
2026-08-28 09:09:04 +0900

    fix: remove unused timer input helper

--- kernel/timer.c
+++ kernel/timer.c
@@ -19,12 +19,6 @@ static void outb(uint16_t port, uint8_t value) {
     __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
 }
 
-static uint8_t inb(uint16_t port) {
-    uint8_t value;
-    __asm__ volatile ("inb %1, %0" : "=a"(value) : "Nd"(port));
-    return value;
-}
-
 static void timer_debug_char(char c) {
     while (1) {
         uint8_t status;
