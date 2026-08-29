4086abad718d611acff6c3490debb05a3ccdb80a
godrenkon <renkomkun@gmail.com>
2026-08-29 18:08:17 +0900

    test(process): add pure TSS stack validation test

---
+++ tests/gdt_stack_host_test.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+#include <stdint.h>
+#include "../kernel/arch/x86_64/gdt.h"
+
+int main(void) {
+    assert(!gdt_kernel_stack_pointer_valid(0u));
+    assert(gdt_kernel_stack_pointer_valid(16u));
+    assert(gdt_kernel_stack_pointer_valid(0x1000u));
+    assert(!gdt_kernel_stack_pointer_valid(1u));
+    assert(!gdt_kernel_stack_pointer_valid(0x1008u));
+    return 0;
+}
