dae863678ba6aa51124e2626aab4f6393276f214
godrenkon <renkomkun@gmail.com>
2026-08-29 17:58:53 +0900

    feat(process): expose user-context entry ABI

--- kernel/arch/x86_64/user_mode.h
+++ kernel/arch/x86_64/user_mode.h
@@ -2,7 +2,9 @@
 #define SB_ARCH_X86_64_USER_MODE_H
 
 #include <stdint.h>
+#include "user_context.h"
 
 void arch_enter_user(uint64_t entry, uint64_t user_stack);
+void arch_enter_user_context(const sb_user_context_t *context);
 
 #endif
