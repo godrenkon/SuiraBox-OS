0ba450bd020f7d4f8de8303fbffec90863be2301
godrenkon <renkomkun@gmail.com>
2026-08-29 18:03:59 +0900

    fix(process): compile-time lock user context ABI layout

--- kernel/arch/x86_64/user_context.h
+++ kernel/arch/x86_64/user_context.h
@@ -2,6 +2,7 @@
 #define SB_ARCH_X86_64_USER_CONTEXT_H
 
 #include <stdint.h>
+#include <stddef.h>
 #include "gdt.h"
 
 #define SB_USER_RFLAGS_RESERVED 0x2u
@@ -31,6 +32,22 @@ typedef struct {
     uint64_t rsp;
     uint64_t ss;
 } sb_user_context_t;
+
+_Static_assert(sizeof(sb_user_context_t) == 160u, "user context ABI size changed");
+_Static_assert(offsetof(sb_user_context_t, r15) == 0u, "user context ABI r15 offset changed");
+_Static_assert(offsetof(sb_user_context_t, r8) == 56u, "user context ABI r8 offset changed");
+_Static_assert(offsetof(sb_user_context_t, rsi) == 64u, "user context ABI rsi offset changed");
+_Static_assert(offsetof(sb_user_context_t, rdi) == 72u, "user context ABI rdi offset changed");
+_Static_assert(offsetof(sb_user_context_t, rbp) == 80u, "user context ABI rbp offset changed");
+_Static_assert(offsetof(sb_user_context_t, rdx) == 88u, "user context ABI rdx offset changed");
+_Static_assert(offsetof(sb_user_context_t, rcx) == 96u, "user context ABI rcx offset changed");
+_Static_assert(offsetof(sb_user_context_t, rbx) == 104u, "user context ABI rbx offset changed");
+_Static_assert(offsetof(sb_user_context_t, rax) == 112u, "user context ABI rax offset changed");
+_Static_assert(offsetof(sb_user_context_t, rip) == 120u, "user context ABI rip offset changed");
+_Static_assert(offsetof(sb_user_context_t, cs) == 128u, "user context ABI cs offset changed");
+_Static_assert(offsetof(sb_user_context_t, rflags) == 136u, "user context ABI rflags offset changed");
+_Static_assert(offsetof(sb_user_context_t, rsp) == 144u, "user context ABI rsp offset changed");
+_Static_assert(offsetof(sb_user_context_t, ss) == 152u, "user context ABI ss offset changed");
 
 int sb_user_context_init(sb_user_context_t *context,
                          uint64_t entry_point,
