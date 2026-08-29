f4e10309d99052047dffc13a788083d5701845f5
godrenkon <renkomkun@gmail.com>
2026-08-29 15:46:02 +0900

    feat(syscall): add batched glyph-pair ABI

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -15,6 +15,7 @@
 #define SB_SYS_CONFIG_GET      9u
 #define SB_SYS_CONFIG_SET      10u
 #define SB_SYS_YIELD           11u
+#define SB_SYS_DISPLAY_GLYPH_PAIR 12u
 #define SB_CONFIG_SET_VOLATILE 1u
 
 void syscall_init(void);
