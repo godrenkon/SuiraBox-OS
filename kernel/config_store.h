b9bf77fb25895f3cc4fad37ae140845d8d6ce2e8
godrenkon <renkomkun@gmail.com>
2026-08-29 16:39:59 +0900

    feat(config): persist optional resource feature mask

--- kernel/config_store.h
+++ kernel/config_store.h
@@ -7,6 +7,7 @@
 #define SB_CONFIG_STORE_VERSION 1u
 #define SB_CONFIG_STORE_COMPLETED 1u
 #define SB_CONFIG_STORE_RECORD_SIZE 32u
+#define SB_CONFIG_OPTIONAL_MASK_ALL_SUPPORTED 0x0000001Fu
 
 typedef struct __attribute__((packed)) {
     uint32_t magic;
@@ -15,10 +16,11 @@ typedef struct __attribute__((packed)) {
     uint8_t completed;
     uint32_t generation;
     uint32_t checksum;
-    uint8_t reserved[16];
+    uint32_t optional_enabled_mask;
+    uint8_t reserved[12];
 } sb_config_store_record_t;
 
 int sb_config_store_get(sb_config_store_record_t *record);
-int sb_config_store_set(uint8_t language);
+int sb_config_store_set(uint8_t language, uint32_t optional_enabled_mask);
 
 #endif
