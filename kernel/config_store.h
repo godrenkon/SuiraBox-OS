c03eed9db24c13fd019b5268166684aeb52609d5
godrenkon <renkomkun@gmail.com>
2026-08-29 15:10:06 +0900

    feat(config): add kernel-side persistent config store

---
+++ kernel/config_store.h
@@ -0,0 +1,24 @@
+#ifndef SB_KERNEL_CONFIG_STORE_H
+#define SB_KERNEL_CONFIG_STORE_H
+
+#include <stdint.h>
+
+#define SB_CONFIG_STORE_MAGIC 0x53424346u
+#define SB_CONFIG_STORE_VERSION 1u
+#define SB_CONFIG_STORE_COMPLETED 1u
+#define SB_CONFIG_STORE_RECORD_SIZE 32u
+
+typedef struct __attribute__((packed)) {
+    uint32_t magic;
+    uint16_t version;
+    uint8_t language;
+    uint8_t completed;
+    uint32_t generation;
+    uint32_t checksum;
+    uint8_t reserved[16];
+} sb_config_store_record_t;
+
+int sb_config_store_get(sb_config_store_record_t *record);
+int sb_config_store_set(uint8_t language);
+
+#endif
