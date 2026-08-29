431580e7521a3f0c005932c7e8ba69efe7c811d1
godrenkon <renkomkun@gmail.com>
2026-08-29 15:10:36 +0900

    feat(syscall): add persistent config ABI

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -12,6 +12,8 @@
 #define SB_SYS_INPUT_KEY       6u
 #define SB_SYS_DISPLAY_GLYPH   7u
 #define SB_SYS_INPUT_MOUSE     8u
+#define SB_SYS_CONFIG_GET      9u
+#define SB_SYS_CONFIG_SET      10u
 
 void syscall_init(void);
 uint64_t syscall_dispatch(uint64_t number, uint64_t arg0, uint64_t arg1,
