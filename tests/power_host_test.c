eb43be4046ad325cab051dda509ebfdb022d6379
godrenkon <renkomkun@gmail.com>
2026-08-30 16:31:50 +0900

    test(power): isolate ACPI device registration in host test

--- tests/power_host_test.c
+++ tests/power_host_test.c
@@ -1,5 +1,23 @@
 #include <assert.h>
 #include "../kernel/power.h"
+#include "../kernel/device.h"
+
+static sb_device_t test_power_device;
+
+sb_device_t *sb_device_register(sb_device_bus_t bus, sb_device_class_t class_id,
+                                uint16_t vendor_id, uint16_t device_id,
+                                const char *name) {
+    (void)bus;
+    (void)vendor_id;
+    (void)device_id;
+    (void)name;
+    test_power_device = (sb_device_t){
+        .index = 0u,
+        .class_id = class_id,
+        .state = SB_DEVICE_DISCOVERED
+    };
+    return &test_power_device;
+}
 
 int main(void) {
     sb_power_init();
