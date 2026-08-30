91ff19a12c1899a49f19e8092d37dbe68f254c4b
godrenkon <renkomkun@gmail.com>
2026-08-30 12:11:57 +0900

    feat(acpi): expose FADT power-control metadata

--- kernel/acpi.h
+++ kernel/acpi.h
@@ -5,13 +5,23 @@
 
 typedef struct {
     uint64_t rsdp_address;
+    uint64_t root_table_address;
+    uint64_t fadt_address;
+    uint64_t dsdt_address;
+    uint32_t pm1a_control_block;
+    uint32_t pm1b_control_block;
+    uint8_t pm1_control_length;
+    uint8_t sleep_type_a;
+    uint8_t sleep_type_b;
     uint8_t revision;
     uint32_t length;
     uint8_t valid;
+    uint8_t power_control_valid;
 } sb_acpi_info_t;
 
 int sb_acpi_init_from_multiboot(uint64_t multiboot_info);
 const sb_acpi_info_t *sb_acpi_info(void);
 int sb_acpi_available(void);
+int sb_acpi_poweroff(void);
 
 #endif
