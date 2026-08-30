1a360d1f2daf41542fde1cea14368ca3d0bf6c9e
godrenkon <renkomkun@gmail.com>
2026-08-30 10:14:05 +0900

    process: support removal of non-tail user threads

--- kernel/user_scheduler.h
+++ kernel/user_scheduler.h
@@ -11,6 +11,7 @@
 void user_scheduler_init(void);
 int user_scheduler_add(sb_process_t *process, sb_thread_t *thread);
 int user_scheduler_remove(sb_process_t *process, sb_thread_t *thread);
+int user_scheduler_rebind_thread(sb_process_t *process, sb_thread_t *old_thread, sb_thread_t *new_thread);
 int user_scheduler_set_current(sb_process_t *process, sb_thread_t *thread);
 sb_thread_t *user_scheduler_current_thread(void);
 sb_process_t *user_scheduler_current_process(void);
