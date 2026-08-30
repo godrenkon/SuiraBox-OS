a9f16d8bfe36d8cd0ba0334d608fa16c1a147a1d
godrenkon <renkomkun@gmail.com>
2026-08-30 12:49:33 +0900

    feat(storage): expose filesystem storage synchronization hook

--- kernel/storage.h
+++ kernel/storage.h
@@ -2,9 +2,11 @@
 #define SB_KERNEL_STORAGE_H
 
 #include "fs/fat32.h"
+#include "block.h"
 
 int sb_storage_init(void);
 int sb_storage_ready(void);
 sb_fat32_t *sb_storage_fat32(void);
+sb_block_status_t sb_storage_sync(void);
 
 #endif
