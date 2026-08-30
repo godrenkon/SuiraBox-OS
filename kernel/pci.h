78592bbdfe2608e7d532fce2b4a7f1a4f67a5eac
godrenkon <renkomkun@gmail.com>
2026-08-30 13:27:02 +0900

    feat(pci): expose safe config write and command bits

--- kernel/pci.h
+++ kernel/pci.h
@@ -16,12 +16,17 @@
 #define PCI_CLASS_MULTIMEDIA   0x04u
 #define PCI_CLASS_BRIDGE       0x06u
 
+#define PCI_COMMAND_IO_SPACE      0x0001u
+#define PCI_COMMAND_MEMORY_SPACE  0x0002u
+#define PCI_COMMAND_BUS_MASTER    0x0004u
+
 #define PCI_CAP_ID_MSI  0x05u
 #define PCI_CAP_ID_MSIX 0x11u
 #define PCI_CAP_ID_PM   0x01u
 
 uint32_t pci_config_read32(uint8_t bus, uint8_t device, uint8_t function, uint8_t offset);
 uint16_t pci_config_read16(uint8_t bus, uint8_t device, uint8_t function, uint8_t offset);
+void pci_config_write16(uint8_t bus, uint8_t device, uint8_t function, uint8_t offset, uint16_t value);
 int pci_find_capability(uint8_t bus, uint8_t device, uint8_t function, uint8_t capability_id, uint8_t *offset_out);
 void pci_enumerate(void);
 
