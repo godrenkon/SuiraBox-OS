0943afeb90d4d4151dcbe12e6725962dfcb2c05b
godrenkon <renkomkun@gmail.com>
2026-08-30 16:23:46 +0900

    test(net): isolate stack host test from hardware backend

--- tests/net_stack_host_test.c
+++ tests/net_stack_host_test.c
@@ -1,6 +1,31 @@
 #include <assert.h>
 #include <stdint.h>
 #include "../kernel/net_stack.h"
+#include "../kernel/net_device.h"
+
+uint32_t sb_net_device_count(void) {
+    return 0u;
+}
+
+const sb_net_device_t *sb_net_device_get(uint32_t index) {
+    (void)index;
+    return 0;
+}
+
+int sb_net_device_send(uint32_t index, const uint8_t *frame, uint16_t length) {
+    (void)index;
+    (void)frame;
+    (void)length;
+    return -1;
+}
+
+int sb_net_device_receive(uint32_t index, uint8_t *frame, uint16_t capacity, uint16_t *length) {
+    (void)index;
+    (void)frame;
+    (void)capacity;
+    (void)length;
+    return -1;
+}
 
 static void write16be(uint8_t *p, uint16_t value) {
     p[0] = (uint8_t)(value >> 8);
