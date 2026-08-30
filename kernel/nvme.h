791cb148f8983c84861f8ddfbf39f900e084819c
godrenkon <renkomkun@gmail.com>
2026-08-30 12:14:05 +0900

    feat(storage): expose NVMe controller capability registers

--- kernel/nvme.h
+++ kernel/nvme.h
@@ -9,7 +9,14 @@ typedef struct {
     uint32_t device_index;
     uint16_t vendor_id;
     uint16_t device_id;
+    uint64_t mmio_base;
+    uint32_t version;
+    uint32_t max_queue_entries;
+    uint16_t max_submission_queue_entries;
+    uint16_t max_completion_queue_entries;
+    uint32_t controller_status;
     uint8_t ready;
+    uint8_t mmio_valid;
 } sb_nvme_controller_t;
 
 void sb_nvme_init(void);
