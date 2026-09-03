865cf7e4c60428a9733ebd6448ca2d48f8617ae4
godrenkon <renkomkun@gmail.com>
2026-09-03 21:04:38 +0900

    Fix file handle host FAT32 list stub

--- tests/file_handle_host_fat32_stub.c
+++ tests/file_handle_host_fat32_stub.c
@@ -6,3 +6,12 @@ int sb_fat32_read_root_entry(sb_fat32_t *fs, uint32_t index, sb_fat32_dirent_t *
     (void)entry;
     return 0;
 }
+
+int sb_fat32_read_directory_entry(sb_fat32_t *fs, uint32_t directory_cluster,
+                                  uint32_t index, sb_fat32_dirent_t *entry) {
+    (void)fs;
+    (void)directory_cluster;
+    (void)index;
+    (void)entry;
+    return 0;
+}
