fb1dc2d272e5fccb3896db24dbc846bbc0c851dd
godrenkon <renkomkun@gmail.com>
2026-09-05 08:12:15 +0900

    Declare timer IRQ dispatch ABI

--- kernel/timer.h
+++ kernel/timer.h
@@ -3,11 +3,14 @@
 
 #include <stdint.h>
 
+struct sb_timer_saved_gpr;
+
 /* Default kernel timer used by the bootstrap path. */
 void timer_init(void);
 /* Configurable PIT timer for later policy/runtime use. */
 void timer_init_frequency(uint32_t frequency_hz);
 uint64_t timer_ticks(void);
 void sb_timer_tick(void);
+uintptr_t sb_timer_irq_dispatch(struct sb_timer_saved_gpr *gpr);
 
 #endif
