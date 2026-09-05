7da6a61cb56313f879a0a174efe5f60e9f7ab7c1
godrenkon <renkomkun@gmail.com>
2026-09-05 21:03:13 +0900

    scheduler: expose explicit userspace yield switch

--- kernel/user_scheduler.h
+++ kernel/user_scheduler.h
@@ -16,12 +16,14 @@ int user_scheduler_set_current(sb_process_t *process, sb_thread_t *thread);
 int user_scheduler_request_exit(sb_process_t *process, sb_thread_t *thread);
 int user_scheduler_sleep_thread(sb_process_t *process, sb_thread_t *thread, uint64_t wake_tick);
 int user_scheduler_request_sleep(uint64_t wake_tick);
+int user_scheduler_request_yield(void);
 uint32_t user_scheduler_wake_expired(uint64_t now_tick);
 sb_thread_t *user_scheduler_current_thread(void);
 sb_process_t *user_scheduler_current_process(void);
 uint32_t user_scheduler_count(void);
 uintptr_t user_scheduler_exit_dispatch(void);
 uintptr_t user_scheduler_sleep_dispatch(uintptr_t saved_gpr_base);
+uintptr_t user_scheduler_yield_dispatch(uintptr_t saved_gpr_base);
 uintptr_t user_scheduler_timer_dispatch(sb_timer_saved_gpr_t *gpr);
 
 #endif
