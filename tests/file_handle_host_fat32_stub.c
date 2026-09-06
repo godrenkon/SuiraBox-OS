84ee9e281897c66c87980094ac8723c84697dd5e
godrenkon <renkomkun@gmail.com>
2026-09-06 19:48:18 +0900

    make FAT32 refresh stub succeed in file handle host test

--- tests/file_handle_host_fat32_stub.c
+++ tests/file_handle_host_fat32_stub.c
@@ -19,5 +19,5 @@ int sb_fat32_read_directory_entry(sb_fat32_t *fs, uint32_t directory_cluster,
 int sb_fat32_refresh_dirent(sb_fat32_t *fs, sb_fat32_dirent_t *entry) {
     (void)fs;
     (void)entry;
-    return 0;
+    return 1;
 }
