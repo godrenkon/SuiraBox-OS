a61bc8081e5e65838d19a57f95f5f0bf8b5cb451
godrenkon <renkomkun@gmail.com>
2026-09-06 17:24:17 +0900

    feat(app): add caller-scoped application lifecycle APIs

--- kernel/app_manager.h
+++ kernel/app_manager.h
@@ -10,7 +10,9 @@
 void sb_app_manager_init(uint64_t multiboot_info);
 int sb_app_launch(uint32_t app_id);
 int sb_app_terminate(uint32_t app_id, uint64_t exit_code);
+int sb_app_terminate_for_current(uint32_t app_id, uint64_t exit_code);
 int sb_app_is_running(uint32_t app_id);
+int sb_app_is_running_for_current(uint32_t app_id);
 uint32_t sb_app_reap_exited(void);
 uint32_t sb_app_count(void);
 
